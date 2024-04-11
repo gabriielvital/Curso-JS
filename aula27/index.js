e3ca2bca1e8275f07cfccddd7a55a8191b9d11f1
Gabriel Vital <gabrit_8@hotmail.com>
2024-04-11 17:04:01 -0300

    Primeiro Commit

---
+++ aula27/index.js
@@ -0,0 +1,14 @@
+// (condição) ? 'Valor para verdadeira' : 'Valor para falso';
+
+const pontuacaoUsuario = 500;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUsuario);
+
+/* 
+if(pontuacaoUsuario >= 1000){
+    console.log('Usuario VIP');
+}else{
+    console.log('Usuario normal');
+}
+
+*/
