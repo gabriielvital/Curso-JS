f2c03c4fc6dc675786fe0ea8eda9bdb6e344f8a7
Gabriel Vital <gabrit_8@hotmail.com>
2024-04-11 17:22:10 -0300

    Operação ternaria e fallback
    Aqui temos uma versão de um if reduzida, como uma operação ternária, e temos fallback como um setador de padrões, que caso um valor seja nulo ou indefinido, é setado um valor padrão

--- aula27/index.js
+++ aula27/index.js
@@ -1,9 +1,3 @@
-// (condição) ? 'Valor para verdadeira' : 'Valor para falso';
-
-const pontuacaoUsuario = 500;
-const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
-console.log(nivelUsuario);
-
 /* 
 if(pontuacaoUsuario >= 1000){
     console.log('Usuario VIP');
@@ -12,3 +6,17 @@ if(pontuacaoUsuario >= 1000){
 }
 
 */
+
+// (condição) ? 'Valor para verdadeira' : 'Valor para falso';
+
+const pontuacaoUsuario = 500;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preta';
+
+console.log(nivelUsuario, corPadrao);
+
+
+
